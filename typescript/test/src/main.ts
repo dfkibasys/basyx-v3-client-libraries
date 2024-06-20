264bce63588f1a965b9530060ed801d6c7004f2b
Jonas Mohr <jonas.mohr@dfki.de>
2024-06-20 14:00:21 +0200

    Working Typescript test

---
+++ typescript/test/src/main.ts
@@ -0,0 +1,16 @@
+import {createConfiguration, AssetAdministrationShellRegistryApi, ServerConfiguration } from "@basys/v3-client-ts";
+
+function main(): void {
+  let conf = {
+    baseServer: new ServerConfiguration<{  }>("http://aas-registry-v3.local.lan", {  })
+  }
+
+  const configuration = createConfiguration(conf);
+  const apiInstance = new AssetAdministrationShellRegistryApi(configuration);
+
+  apiInstance.getAllAssetAdministrationShellDescriptors( ).then((data:any) => {
+    console.log('API called successfully. Returned data: ' + data);
+  }).catch((error:any) => console.error(error));
+}
+
+main();
